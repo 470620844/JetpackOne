64d71d15060bc0c7d1518bd4ac371a259d645466
何飞 <hefei@zhuomogroup.com>
2020-06-08 21:00:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jetpackone/ui/main/JitPackTest.java
@@ -0,0 +1,9 @@
+package com.example.jetpackone.ui.main;
+
+/**
+ * description ：
+ * author : hf
+ * date : 2020/6/8 20:57
+ */
+public class JitPackTest {
+}
---
+++ app/src/main/java/com/example/jetpackone/ui/main/MainViewModel.java
@@ -0,0 +1,13 @@
+package com.example.jetpackone.ui.main;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+
+
+    private MutableLiveData mData = new MutableLiveData<String>();
+
+
+}
